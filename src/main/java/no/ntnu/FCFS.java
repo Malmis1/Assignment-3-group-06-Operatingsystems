262498992ed75ae16b8a55d25d22d05f47984106
Andreas Malmedal <andreas07.malmedal@gmail.com>
2024-03-19 11:59:17 +0100

    Added some methods for FCFS.
    Added field for processes.
    Added method for adding process.
    Added method for getting processes sorted by arrival time.

--- src/main/java/no/ntnu/FCFS.java
+++ src/main/java/no/ntnu/FCFS.java
@@ -1,5 +1,60 @@
 package no.ntnu;
 
-public class FCFS {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Represents a First-Come-First-Serve CPU scheduling algorithm.
+ */
+public class FCFS {
+    private List<CPUProcess> processes;
+
+    /**
+     * Creates a new instance of {@code FCFS}.
+     */
+    public FCFS() {
+        this.processes = new ArrayList<>();
+    }
+
+    /**
+     * Adds a process to be handled by the scheduling algorithm.
+     * 
+     * @param id          the id of the process.
+     * @param arrivalTime the arrival time of the process.
+     * @param burstTime   the burst time of the process.
+     */
+    public void addProcess(CPUProcess process) {
+        this.processes.add(process);
+    }
+
+    /**
+     * Gets a list of all the processes sorted by arrival times in an ascending
+     * order.
+     * 
+     * @return a list of all the processes sorted by arrival times.
+     * @throws IllegalStateException if there are no processes added.
+     */
+    private List<CPUProcess> getSortedQueue() throws IllegalStateException {
+        if (this.processes.size() == 0) {
+            throw new IllegalStateException("there must be at least one process");
+        }
+
+        List<CPUProcess> queue = new ArrayList<>();
+        List<CPUProcess> remainingProcesses = this.processes;
+        CPUProcess firstArrival = null;
+        while (remainingProcesses.size() != 0) {
+            for (CPUProcess process : this.processes) {
+                if (remainingProcesses.contains(process)) {
+                    if (firstArrival == null || process.getArrivalTime() < firstArrival.getArrivalTime()) {
+                        firstArrival = process;
+                    }
+                }
+            }
+            queue.add(firstArrival);
+            remainingProcesses.remove(firstArrival);
+            firstArrival = null;
+        }
+
+        return queue;
+    }
 }
